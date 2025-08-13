be7197f59bd68463851ed7fe5daedc6dcf7f5af0
Jimmy Chemuku Nyongesa <jimmycn03@gmail.com>
2025-08-13 15:07:08 +0300

    feat: Added a logic to allow mounting/unmounting the sidebar on smaller devices

--- portfolio/src/pages/GUI/Gui.jsx
+++ portfolio/src/pages/GUI/Gui.jsx
@@ -5,31 +5,42 @@ import { BsDatabaseGear } from 'react-icons/bs';
 import { TbFileCertificate } from 'react-icons/tb';
 import { useState } from 'react';
 import Home from '../../views/Home/Home.jsx';
+import Header from '../../components/Header.jsx';
 
 function Gui () {
   const [view, setView] = useState(<Home />);
+  const [showSidebar, setShowSidebar] = useState(false);
   return (
     <div className={styles.background}>
-      <Sidebar buttons={[
-        { name: 'Home', action: () => setView(<Home />), icon: <FaHome /> },
-        {
-          name: 'About',
-          action: () => alert('Feature to be developed soon!'),
-          icon: <FaUser />
-        },
-        {
-          name: 'Achievements',
-          action: () => alert('Feature to be developed soon!'),
-          icon: <TbFileCertificate />
-        },
-        {
-          name: 'Projects',
-          action: () => alert('Feature to be developed soon!'),
-          icon: <BsDatabaseGear />
-        }
-      ]}
-      />
-      <section className={styles.viewSection}>{view}</section>
+      <Header openSide={() => setShowSidebar(prev => !prev)} />
+      {showSidebar &&
+        <Sidebar buttons={[
+          { name: 'Home', action: () => setView(<Home />), icon: <FaHome /> },
+          {
+            name: 'About',
+            action: () => alert('Feature to be developed soon!'),
+            icon: <FaUser />
+          },
+          {
+            name: 'Achievements',
+            action: () => alert('Feature to be developed soon!'),
+            icon: <TbFileCertificate />
+          },
+          {
+            name: 'Projects',
+            action: () => alert('Feature to be developed soon!'),
+            icon: <BsDatabaseGear />
+          }
+        ]}
+        />}
+      <section
+        className={styles.viewSection} onClick={() => {
+          if (window.length < 768) {
+            setShowSidebar(false);
+          }
+        }}
+      >{view}
+      </section>
     </div>
   );
 }
