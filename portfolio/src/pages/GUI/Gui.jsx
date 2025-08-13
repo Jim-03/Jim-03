577da5561c170d1f2559000e7a5d6e686ec9ae71
Jimmy Chemuku Nyongesa <jimmycn03@gmail.com>
2025-08-13 16:26:00 +0300

    refactor: Added a useEffect hook to mount/unmount the sidebar depending on the screen size

--- portfolio/src/pages/GUI/Gui.jsx
+++ portfolio/src/pages/GUI/Gui.jsx
@@ -3,13 +3,29 @@ import styles from './Gui.module.css';
 import { FaHome, FaUser } from 'react-icons/fa';
 import { BsDatabaseGear } from 'react-icons/bs';
 import { TbFileCertificate } from 'react-icons/tb';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Home from '../../views/Home/Home.jsx';
 import Header from '../../components/Header.jsx';
 
 function Gui () {
   const [view, setView] = useState(<Home />);
-  const [showSidebar, setShowSidebar] = useState(false);
+  const [showSidebar, setShowSidebar] = useState(true);
+  const [windowSize, setWindowSize] = useState(window.innerWidth);
+
+  useEffect(() => {
+    function changeOnResize () {
+      setWindowSize(window.innerWidth);
+    }
+
+    window.addEventListener('resize', changeOnResize);
+    if (windowSize <= 768) {
+      setShowSidebar(false);
+    } else {
+      setShowSidebar(true);
+    }
+    return () => window.removeEventListener('resize', changeOnResize);
+  }, [windowSize]);
+
   return (
     <div className={styles.background}>
       <Header openSide={() => setShowSidebar(prev => !prev)} />
@@ -35,7 +51,7 @@ function Gui () {
         />}
       <section
         className={styles.viewSection} onClick={() => {
-          if (window.length < 768) {
+          if (windowSize < 768) {
             setShowSidebar(false);
           }
         }}
